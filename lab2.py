3d36cdad6cfaf2b3eb2db1e1b7c44fc43e2a0e50
jemraider21 <jemraider21@gmail.com>
2023-01-19 17:47:52 -0500

    Created validation for generating a password if the user decides no on all options. Refactored code for ease of use

--- lab2.py
+++ lab2.py
@@ -3,7 +3,7 @@ import datetime
 import math
 import random
 import string
-from validation import InvalidInputException, validate_is_number, validate_is_boolean
+from validation import validate_is_number, validate_is_boolean
 
 
 def menu() -> int:
@@ -23,45 +23,57 @@ def menu() -> int:
     return user_input
 
 
-def generate_password() -> str:
+def adding_characters(characters: str, use: bool, to_add: str) -> str:
+    return characters + to_add if use else characters
+
+
+def generate_password():
+    print("\nGenerating secure password")
     characters: str = ""
     additional_info = "True/False"
     length = validate_is_number("length of password")
     use_lowercase: bool = validate_is_boolean(
         "boolean for lower case letters", additional_info)
-    if use_lowercase:
-        characters += string.ascii_lowercase
     use_uppercase: bool = validate_is_boolean(
         "boolean for upper case letters", additional_info)
-    if use_uppercase:
-        characters += string.ascii_uppercase
     use_numbers: bool = validate_is_boolean(
         "boolean for numbers", additional_info)
-    if use_numbers:
-        characters += string.digits
     use_specials = validate_is_boolean(
         "boolean for special characters", additional_info)
-    if use_specials:
-        characters += string.punctuation
-    return "".join(random.choice(characters) for i in range(length))
+
+    if all([use_lowercase, use_uppercase, use_numbers, use_specials]) == False:
+        characters = string.ascii_letters
+    else:
+        characters = adding_characters(
+            characters, use_lowercase, string.ascii_lowercase)
+        characters = adding_characters(
+            characters, use_uppercase, string.ascii_uppercase)
+        characters = adding_characters(characters, use_numbers, string.digits)
+        characters = adding_characters(
+            characters, use_specials, string.punctuation)
+    password: str = "".join(random.choice(characters) for i in range(length))
+    print(f'Your new generated password: {password}\n')
 
 
-def calculate_format_percentage() -> float:
+def calculate_format_percentage():
+    print("\nCalculating and formating a percentage")
     numerator: int = validate_is_number("Numerator")
     denominator: int = validate_is_number("Denominator")
     result: float = (numerator/denominator) * 100
     decimal: int = validate_is_number("Number of Decimals")
-    return round(result, decimal)
+    print(f'The calculated percentage: {round(result, decimal)} \n')
 
 
 def days_until_july_4_2025() -> int:
+    print("\nDays until july 4 2025")
     today: date = datetime.date.today()
     future: date = datetime.date(2025, 7, 4)
     difference: timedelta = future - today
-    return difference.days
+    print(f'There are {difference.days} days until July 4, 2025\n')
 
 
 def law_of_cosines():
+    print("\nUsing the law of cosine")
     side_a: float = validate_is_number("Side A", number_type="float")
     side_b: float = validate_is_number("Side B", number_type="float")
     angle_c: float = validate_is_number("Angle C", number_type="float")
@@ -69,46 +81,35 @@ def law_of_cosines():
     power_calcs = math.pow(side_a, 2) + math.pow(side_b, 2)
     cosine_cals = 2 * side_a * side_b * math.cos(angle_c)
     side_c: float = math.sqrt(power_calcs - cosine_cals)
-    return side_c
+    print(f'The length of Side C: {round(side_c, 2)}\n')
 
 
-def volume_right_circular_cylinder() -> float:
+def volume_right_circular_cylinder():
+    print("\nCalculating Volume of a Right Circular Cylinder")
     radius: float = validate_is_number("Radius", number_type="float")
     height: float = validate_is_number("Height", number_type="float")
     volume: float = math.pi * math.pow(radius, 2) * height
-    return volume
+    print(f'The volume of the right circular cylinder: {round(volume, 2)}\n')
 
 
 def main():
     print("Welcome to the program!\n")
     menu_loop = True
     while menu_loop:
-        try:
-            selection = menu()
-            match selection:
-                case 1:
-                    print("\nGenerating secure password")
-                    print(
-                        f'Your new generated password: {generate_password()}\n')
-                case 2:
-                    print("\nCalculating and formating a percentage")
-                    print(
-                        f'The calculated percentage: {calculate_format_percentage()} \n')
-                case 3:
-                    print("\nDays until july 4 2025")
-                    print(
-                        f'There are {days_until_july_4_2025()} days until July 4, 2025\n')
-                case 4:
-                    print("\nUsing the law of cosine")
-                    print(f'The length of Side C: {law_of_cosines()}\n')
-                case 5:
-                    print("\nCalculating Volume of a Right Circular Cylinder")
-                    print(
-                        f'The volume of the right circular cylinder: {volume_right_circular_cylinder()}\n')
-                case 6:
-                    menu_loop = False
-        except InvalidInputException as exception:
-            print(f'{exception.__class__.__name__}: {exception}\n')
+        selection = menu()
+        match selection:
+            case 1:
+                generate_password()
+            case 2:
+                calculate_format_percentage()
+            case 3:
+                days_until_july_4_2025()
+            case 4:
+                law_of_cosines()
+            case 5:
+                volume_right_circular_cylinder()
+            case 6:
+                menu_loop = False
     print("\nThank you for using the program!")
 
 
