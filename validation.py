1f1ec5faea23473998bab29ce1f9673b5955eace
jemraider21 <jemraider21@gmail.com>
2023-01-22 13:51:11 -0500

    lab2.py - 10: no changes. validation.py - 9.80: Added docstrings and fixed formatting. Both - 9.92

--- validation.py
+++ validation.py
@@ -1,17 +1,50 @@
+"""A module that contains common validation functions and exceptions
+
+Raises:
+    InvalidInputException: User entered something not expected, so throw an exception
+"""
 from typing import Union
 
 
 class InvalidInputException(Exception):
+    """An exception to throw in the case an unexpected user input was given
+
+    Args:
+        Exception (string): The message of the exception
+    """
+
     def __init__(self, message) -> None:
         super().__init__(message)
 
 
 def conversion_error_message(exception: Exception, input_name: str, typing: str) -> str:
+    """Create an error message explaining that
+    converting a given input to a given type was not successful
+
+    Args:
+        exception (Exception): the thrown exception to generate the message
+        input_name (str): The name of the input (ex: First Name)
+        typing (str): The type used for conversion (ex: string, int)
+
+    Returns:
+        str: The error message
+    """
     error_name = exception.__class__.__name__
     print(f'{error_name}: Cannot convert {input_name} to a {typing}. Try again')
 
 
 def validate_input_not_blank(input_name: str, additional_info: str = "") -> str:
+    """Collect input from the user and ensure it was not blank or empty
+
+    Args:
+        input_name (str): Name of the input (ex: First Name)
+
+        additional_info (str, optional):
+        Any additional information for prompt (ex: units of measurement).Defaults to "".
+
+    Returns:
+        str: the validated and correct user input
+    """
     looper: bool = True
     user_input: str = ""
     while looper:
@@ -29,7 +62,18 @@ def validate_input_not_blank(input_name: str, additional_info: str = "") -> str:
     return user_input
 
 
-def validate_is_boolean(input_name: str, additional_info: str = ""):
+def validate_is_boolean(input_name: str, additional_info: str = "") -> bool:
+    """Validate that the collected user input is both not blank and can be converted to a boolean
+
+    Args:
+        input_name (str): Name of the input (ex: First Name)
+
+        additional_info (str, optional):
+        Any additional information for prompt (ex: units of measurement).Defaults to "".
+
+    Returns:
+        bool: The user input
+    """
     user_input: bool = True
     while True:
         user_input = validate_input_not_blank(input_name, additional_info)
@@ -48,6 +92,22 @@ def validate_is_boolean(input_name: str, additional_info: str = ""):
 
 def validate_is_number(input_name: str, additional_info: str = "",
                        number_type: str = "int") -> Union[int, float]:
+    """Validate that the user input is not blank and it can be converted to a number, being an int or a float
+
+    Args:
+        input_name (str): Name of the input (ex: First Name)
+
+        additional_info (str, optional):
+        Any additional information for prompt (ex: units of measurement).Defaults to "".
+
+        number_type (str, optional): The number data type to convert to. Defaults to "int".
+
+    Raises:
+        InvalidInputException: Incorrect or unexpected user input
+
+    Returns:
+        Union[int, float]: The user input
+    """
     user_input: int = 0
     looper: bool = True
     match number_type:
